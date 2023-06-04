f520b4b000b8b07fd7accb91d369349fde020dde
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-06-04 17:50:57 +0200

    No idea

--- src/main/java/hu/rivalsnetwork/rivalsislands/island/Island.java
+++ src/main/java/hu/rivalsnetwork/rivalsislands/island/Island.java
@@ -1,16 +1,25 @@
 package hu.rivalsnetwork.rivalsislands.island;
 
-import hu.rivalsnetwork.rivalsapi.users.User;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class Island {
-    private User owner;
-    private int x_max = 0;
-    private int x_min = 0;
-    private int z_max = 0;
-    private int z_min = 0;
+import java.util.UUID;
 
-    public Island(@NotNull final User owner) {
-        this.owner = owner;
+public class Island {
+    @BsonProperty(value = "owner_name")
+    private String ownerName;
+    @BsonProperty(value = "island_name")
+    private String islandName;
+    @BsonProperty(value = "owner_uuid")
+    private UUID ownerUUID;
+    private int x_max = 150;
+    private int x_min = 150;
+    private int z_max = 150;
+    private int z_min = 150;
+
+    public Island(@NotNull final Player owner) {
+        this.ownerName = owner.getName();
+        this.ownerUUID = owner.getUniqueId();
     }
 }
